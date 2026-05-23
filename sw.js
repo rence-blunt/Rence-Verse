6a3bbeb7ad9ac3be8727c03c369f58108a4f787e
rence-blunt <velkovixhub23@gmail.com>
2026-05-23 11:35:00 +0000

    maxi

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'rence-verse-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'images/renceverse_logo.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
